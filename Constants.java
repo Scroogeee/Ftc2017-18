000d0a6c9cd2206dcacd1d208307fdb510535d7b
Scroogeee <30441849+Scroogeee@users.noreply.github.com>
2017-10-20 16:05:30 +0200

    Finished Implementation for the Glyph Arm
    + Removed the slow driving activation (Always fast)

---
+++ Constants.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.preJC_DriverControlled;
+
+/**
+ * Created by FTC on 16.10.2017.
+ */
+public class Constants {
+    /**
+     * Variables for the names of the components
+     */
+    public static final String rd_name = "right_drive";
+    public static final String ld_name = "left_drive";
+    public static final String servoGlyph_name = "servo_glyph";
+    public static final double FAST_SCALE = 1;
+    public static final double SLOW_SCALE = 0.2;
+
+}
---
+++ GlyphControl.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.preJC_DriverControlled;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by FTC on 17.10.2017.
+ */
+public class GlyphControl {
+
+    /**
+     * OpMode reference
+     */
+    private OpMode mainRef;
+
+
+    /**
+     * Tightloose
+     */
+    private Servo tightloose;
+    private double tightloose_pos = 0;
+
+
+    public void init(Test2Wheel t2w) {
+        tightloose = t2w.hardwareMap.servo.get(Constants.servoGlyph_name);
+        tightloose.setPosition(tightloose_pos);
+        mainRef = t2w;
+    }
+
+    public void updateGlyphArm() {
+        updateTLPos();
+
+    }
+
+    private void updateTLPos() {
+        tightloose_pos += mainRef.gamepad2.right_trigger;
+        tightloose_pos -= mainRef.gamepad2.left_trigger;
+        tightloose_pos = Math.min(tightloose_pos, 1);
+        tightloose_pos = Math.max(tightloose_pos, 0);
+        tightloose.setPosition(tightloose_pos);
+        //TODO remove debugging
+        mainRef.telemetry.addData("tightloose:", tightloose_pos);
+        mainRef.telemetry.update();
+    }
+
+}
