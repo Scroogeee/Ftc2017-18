638addb8863437c04df14bd53b9dd214ba846617
Scroogeee <30441849+Scroogeee@users.noreply.github.com>
2018-02-06 20:09:03 +0100

    finished the overall function of the robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/notNeeded/Test.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.notNeeded;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by FTC on 22.09.2017.
+ */
+public class Test extends OpMode {
+
+
+    DcMotor fahrenRechts;
+    DcMotor fahrenLinks;
+
+    public void init() {
+        fahrenRechts = hardwareMap.dcMotor.get("fahren_rechts");
+        fahrenLinks = hardwareMap.dcMotor.get("fahren_links");
+    }
+
+    public void loop() {
+        fahrenRechts.setPower(gamepad1.right_stick_y);
+        fahrenLinks.setPower(gamepad1.left_stick_y);
+    }
+
+}
